282dbe69cf2ece57e34a2a8f7c71afef7cc4dd6c
RasaC1nta <vicaralfaruq@gmail.com>
2026-04-16 11:36:50 +0700

    fix buku

--- buku/buku.java
+++ buku/buku.java
@@ -16,14 +16,15 @@ public class buku {
     buku.add("Milea");
     buku.add("Perahu Kertas");
     
+    System.out.println("Daftar Buku : ");
     Tampilan(buku);
     Pinjam(buku, "Mahaguru");
+    System.out.println("Daftar Buku Setelah Buku Mahaguru Dipinjam : ");
     Tampilan(buku);
     
     
    }  
     public static void Tampilan(Set<String> buku){
-    System.out.println("Daftar Buku : ");
     for (Iterator<String> iterator = buku.iterator(); iterator.hasNext();) {
         String string = iterator.next();
         System.out.println(string);
